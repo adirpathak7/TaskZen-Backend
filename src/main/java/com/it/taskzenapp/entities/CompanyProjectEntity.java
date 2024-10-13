eab7ffff6168dc0fccb63f2f63eaf653c62f8829
adirpathak7 <adityarpathak7@gmail.com>
2024-10-13 15:36:58 +0530

    db design for company's

--- src/main/java/com/it/taskzenapp/entities/CompanyProjectEntity.java
+++ src/main/java/com/it/taskzenapp/entities/CompanyProjectEntity.java
@@ -4,10 +4,122 @@
  */
 package com.it.taskzenapp.entities;
 
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author Aditya Pathak R
  */
+@Entity
+@Table(name = "company_project_tbl")
 public class CompanyProjectEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int company_project_id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", referencedColumnName = "company_id")
+    private CompanyMasterEntity companyMasterEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "client_id")
+    private ClientMasterEntity clientMasterEntity;
+
+    private String company_project_name;
+    private String details;
+    private String project_picture;
+
+    @ManyToOne
+    @JoinColumn(name = "skill_id", referencedColumnName = "skill_id")
+
+    private String duration;
+    private LocalDateTime createdAt;
+
+    public CompanyProjectEntity() {
+    }
+
+    public CompanyProjectEntity(int company_project_id, CompanyMasterEntity companyMasterEntity, ClientMasterEntity clientMasterEntity, String company_project_name, String details, String project_picture, String duration, LocalDateTime createdAt) {
+        this.company_project_id = company_project_id;
+        this.companyMasterEntity = companyMasterEntity;
+        this.clientMasterEntity = clientMasterEntity;
+        this.company_project_name = company_project_name;
+        this.details = details;
+        this.project_picture = project_picture;
+        this.duration = duration;
+        this.createdAt = createdAt;
+    }
+
+    public int getCompany_project_id() {
+        return company_project_id;
+    }
+
+    public void setCompany_project_id(int company_project_id) {
+        this.company_project_id = company_project_id;
+    }
+
+    public CompanyMasterEntity getCompanyMasterEntity() {
+        return companyMasterEntity;
+    }
+
+    public void setCompanyMasterEntity(CompanyMasterEntity companyMasterEntity) {
+        this.companyMasterEntity = companyMasterEntity;
+    }
+
+    public ClientMasterEntity getClientMasterEntity() {
+        return clientMasterEntity;
+    }
+
+    public void setClientMasterEntity(ClientMasterEntity clientMasterEntity) {
+        this.clientMasterEntity = clientMasterEntity;
+    }
+
+    public String getCompany_project_name() {
+        return company_project_name;
+    }
+
+    public void setCompany_project_name(String company_project_name) {
+        this.company_project_name = company_project_name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getProject_picture() {
+        return project_picture;
+    }
+
+    public void setProject_picture(String project_picture) {
+        this.project_picture = project_picture;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
--- src/main/java/com/it/taskzenapp/entities/CompanyReviewEntity.java
+++ src/main/java/com/it/taskzenapp/entities/CompanyReviewEntity.java
@@ -4,10 +4,97 @@
  */
 package com.it.taskzenapp.entities;
 
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author Aditya Pathak R
  */
+@Entity
+@Table(name = "company_review_id")
 public class CompanyReviewEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int client_review_id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", referencedColumnName = "company_id")
+    private CompanyMasterEntity companyMasterEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "client_id")
+    private ClientMasterEntity clientMasterEntity;
+
+    private String comment;
+    private int rating;
+    private LocalDateTime createdAt;
+
+    public CompanyReviewEntity() {
+    }
+
+    public CompanyReviewEntity(int client_review_id, CompanyMasterEntity companyMasterEntity, ClientMasterEntity clientMasterEntity, String comment, int rating, LocalDateTime createdAt) {
+        this.client_review_id = client_review_id;
+        this.companyMasterEntity = companyMasterEntity;
+        this.clientMasterEntity = clientMasterEntity;
+        this.comment = comment;
+        this.rating = rating;
+        this.createdAt = createdAt;
+    }
+
+    public int getClient_review_id() {
+        return client_review_id;
+    }
+
+    public void setClient_review_id(int client_review_id) {
+        this.client_review_id = client_review_id;
+    }
+
+    public CompanyMasterEntity getCompanyMasterEntity() {
+        return companyMasterEntity;
+    }
+
+    public void setCompanyMasterEntity(CompanyMasterEntity companyMasterEntity) {
+        this.companyMasterEntity = companyMasterEntity;
+    }
+
+    public ClientMasterEntity getClientMasterEntity() {
+        return clientMasterEntity;
+    }
+
+    public void setClientMasterEntity(ClientMasterEntity clientMasterEntity) {
+        this.clientMasterEntity = clientMasterEntity;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
